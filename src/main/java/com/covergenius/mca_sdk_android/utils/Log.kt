35f282b9b1635e9c48d75a813525b6e3527840a1
Xheghun <ysenruf@gmail.com>
2022-09-16 16:12:41 +0100

    :tada: sdk initialization

--- src/main/java/com/covergenius/mca_sdk_android/utils/Log.kt
+++ src/main/java/com/covergenius/mca_sdk_android/utils/Log.kt
@@ -1,4 +1,20 @@
 package com.covergenius.mca_sdk_android.utils
 
-class Log {
+import android.util.Log
+
+/**
+ * Logging Util for MCA SDK with remote config support
+ * */
+object Log {
+    fun d(tag: String, message: String) {
+
+    }
+
+    fun i(tag: String, message: String) {
+
+    }
+
+    fun e(tag: String, message: String, t: Throwable?) {
+        Log.e(tag, message, t)
+    }
 }
