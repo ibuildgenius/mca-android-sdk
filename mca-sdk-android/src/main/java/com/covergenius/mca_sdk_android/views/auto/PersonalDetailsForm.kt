81e85c59b19df389c528925ffea74cdd768e4b9c
Xheghun <ysenruf@gmail.com>
2022-09-26 21:55:50 +0100

    vehicle

--- mca-sdk-android/src/main/java/com/covergenius/mca_sdk_android/views/auto/PersonalDetailsForm.kt
+++ mca-sdk-android/src/main/java/com/covergenius/mca_sdk_android/views/auto/PersonalDetailsForm.kt
@@ -1,5 +1,10 @@
 package com.covergenius.mca_sdk_android.views.auto
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -8,7 +13,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -28,6 +33,16 @@ import com.covergenius.mca_sdk_android.views.composables.TitledTextField
 
 @Composable
 fun AutoPersonalDetailsForm(product: Product) {
+
+
+    var formProgress by remember { mutableStateOf(0) }
+    var hintText by remember { mutableStateOf("Enter details as it appear on legal documents") }
+
+    val animationTime = 200 // miliseconds
+    val animationTimeExit = 0 // miliseconds
+
+
+
     MyCoverTemplate(content = {
         Column(
             Modifier
@@ -48,7 +63,7 @@ fun AutoPersonalDetailsForm(product: Product) {
                 )
                 Box(modifier = Modifier.width(10.dp))
                 Text(
-                    text = "Enter details as it appear on legal documents",
+                    text = hintText,
                     style = MaterialTheme.typography.body1.copy(fontSize = 12.sp),
                     modifier = Modifier
                         .fillMaxWidth()
@@ -56,7 +71,10 @@ fun AutoPersonalDetailsForm(product: Product) {
                 )
             }
 
-            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(10.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(10.dp)
+            ) {
                 Text(
                     "Learn more",
                     style = MaterialTheme.typography.h1.copy(
@@ -68,9 +86,16 @@ fun AutoPersonalDetailsForm(product: Product) {
                 Box(
                     Modifier
                         .fillMaxWidth()
-                        .weight(1f))
-                Text("Underwritten by ", style = MaterialTheme.typography.h1.copy(fontSize = 12.sp, color = colorGrey))
-                Text("AIICO", style = MaterialTheme.typography.h1.copy(colorNavyBlue, fontSize = 12.sp))
+                        .weight(1f)
+                )
+                Text(
+                    "Underwritten by ",
+                    style = MaterialTheme.typography.h1.copy(fontSize = 12.sp, color = colorGrey)
+                )
+                Text(
+                    "AIICO",
+                    style = MaterialTheme.typography.h1.copy(colorNavyBlue, fontSize = 12.sp)
+                )
                 Box(Modifier.width(4.dp))
                 Image(
                     painter = painterResource(id = R.drawable.aiico),
@@ -81,38 +106,103 @@ fun AutoPersonalDetailsForm(product: Product) {
                 )
             }
 
-            Column {
-                TitledTextField(placeholderText = "First Name, Last Name", title = "Name of Plan Owner")
-                TitledTextField(placeholderText = "Enter email address", title = "Email")
-                TitledTextField(placeholderText = "Enter phone number", title = "Phone")
+            Box(Modifier.height(8.dp))
+            AnimatedVisibility(
 
-                Column(
-                    Modifier
-                        .fillMaxHeight()
-                        .weight(1f), verticalArrangement = Arrangement.Center) {
-                    Separator()
-                }
-
-                Row() {
-                    Column(
-                        Modifier
-                            .fillMaxWidth()
-                            .weight(1f)) {
-                        DropdownField(title ="Vehicle Type", options = listOf("Commercial","Personal"))
-                    }
-                    Box(Modifier.width(10.dp))
-                    Column(
-                        Modifier
-                            .fillMaxWidth()
-                            .weight(1f)) {
-                        DropdownField(title ="Kind of Vehicle", options = listOf("Car","Bus", "Trailer", "Motorcycle").sorted())
-                    }
-                }
-
-
-
-                MyCoverButton("Continue", {})
+                modifier = Modifier.fillMaxWidth(),
+                visible = formProgress == 0,
+                enter = slideInHorizontally(
+                    initialOffsetX = { -300 },
+                    animationSpec = tween(
+                        durationMillis = animationTime,
+                        easing = LinearEasing
+                    )
+                ),
+                exit = slideOutHorizontally(
+                    targetOffsetX = { -300 },
+                    animationSpec = tween(
+                        durationMillis = animationTimeExit,
+                        easing = LinearEasing
+                    )
+                )
+            ) {
+                FormOne()
             }
+
+            AnimatedVisibility(
+                modifier = Modifier.fillMaxWidth(),
+                visible = formProgress == 1,
+                enter = slideInHorizontally(
+                    initialOffsetX = { it },
+                    animationSpec = tween(
+                        durationMillis = animationTime,
+                        easing = LinearEasing
+                    )
+                ),
+                exit = slideOutHorizontally(
+                    targetOffsetX = { it },
+                    animationSpec = tween(
+                        durationMillis = animationTimeExit,
+                        easing = LinearEasing
+                    )
+                )
+            ) {
+                FormTwo()
+            }
+
+            MyCoverButton("Continue", onPressed = {
+                if (formProgress == 1) {
+                } else {
+                    formProgress = 1
+                    hintText = "Enter Vehicle details"
+                }
+            })
+
         }
     })
+}
+
+
+@Composable
+fun FormOne() {
+    Column {
+        TitledTextField(
+            placeholderText = "First Name, Last Name",
+            title = "Name of Plan Owner"
+        )
+        TitledTextField(placeholderText = "Enter email address", title = "Email")
+        TitledTextField(placeholderText = "Enter phone number", title = "Phone")
+
+
+    }
+}
+
+@Composable
+fun FormTwo() {
+    Column {
+        Row() {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                DropdownField(
+                    title = "Vehicle Type",
+                    options = listOf("Commercial", "Personal")
+                )
+            }
+            Box(Modifier.width(10.dp))
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                DropdownField(
+                    title = "Kind of Vehicle",
+                    options = listOf("Car", "Bus", "Trailer", "Motorcycle").sorted()
+                )
+            }
+        }
+        TitledTextField(placeholderText = "Enter Vehicle Plate Number", title = "Vehicle Plate No.")
+    }
 }
